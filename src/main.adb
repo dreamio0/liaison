12b1a2a6df4756a5b8935a3c46ac569a76f70681
dreamio0 <142948048+dreamio0@users.noreply.github.com>
2023-08-27 00:08:27 +0200

    add some files

---
+++ src/main.adb
@@ -0,0 +1,12 @@
+
+with Ada.Text_IO;
+procedure Main is
+
+begin
+   --  Insert code here.
+   -- lalala
+   Ada.Text_IO.put("Hi!");
+   Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");Ada.Text_IO.put("Hi!");
+
+--   null;
+end Main;
